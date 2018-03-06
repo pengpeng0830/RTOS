f0acfc2f44b8d9119ab9084a06926d94f693d620
XZP <xingzhipeng2@126.com>
2018-03-06 23:15:56 +0800

    Update TaskSwitch.c

--- uCOS/TaskSwitch.c
+++ uCOS/TaskSwitch.c
@@ -14,27 +14,27 @@ __asm void PendSV_Handler (void)
     IMPORT pTCurrentTask
     IMPORT pTNextTask
 
-    MRS R0, PSP    ;PSP的值给到R0寄存器，
-    CBZ R0, PendSVHander_nosave   ;R0的值若等于0，直接跳转到PendSVHander_nosave（只有第一次执行时，满足该条件）
+    MRS R0, PSP    ;进入异常以后，PSP会自动压栈保存8个寄存器的值。然后PSP的值给R0寄存器
+    CBZ R0, PendSVHander_nosave   ;判断R0的值若等于0，直接跳转到PendSVHander_nosave（只有第一次执行时，满足该条件，因为第一次没有寄存器的值需要保存）
     
-    STMDB R0!, {R4-R11}   ;手动保存R4-R11寄存器，保存到地址值为R0的地方。
+    STMDB R0!, {R4-R11}   ;手动保存R4-R11寄存器，保存到地址值为R0的地方，R0继续压栈8个空间，此后不再压栈，到达栈底
     
     LDR R1, =pTCurrentTask  ;pTCurrentTask的地址保存到R1
     LDR R1, [R1]   ;pTCurrentTask的值保存到R1
-    STR R0, [R1]   ;R0的值存到pTCurrentTask任务
+    STR R0, [R1]   ;R0的值存到pTCurrentTask->stack，即保存了栈底的地址，下次可以根据该地址恢复数据
     
 PendSVHander_nosave
     LDR R0, =pTCurrentTask  ;pTCurrentTask的地址保存到R0
     LDR R1, =pTNextTask     ;pTNextTask的地址保存到R1
     LDR R2, [R1]            ;pTNextTask的值保存到R2
-    STR R2, [R0]            ;pTNextTask的值保存到pTCurrentTask。即实现了设置当前任务的指针
+    STR R2, [R0]            ;pTNextTask的值保存到pTCurrentTask。即实现了设置当前任务的指针为pTNextTask
  
-    LDR R0, [R2]    ;pTNextTask的值保存到R0
-    LDMIA R0!, {R4-R11}   ;恢复R4-R11的值
+    LDR R0, [R2]    ;pTCurrentTask的值保存到R0
+    LDMIA R0!, {R4-R11}   ;通过R0的值，即pTCurrentTask->stack从栈恢复到寄存器R4-R11
  
-    MSR PSP, R0
-    ORR LR, LR, #0x04
-    BX LR
+    MSR PSP, R0     ;修改PSP的值为R0，这样出栈的时候自动恢复另外8个寄存器的起始位置是正确的
+    ORR LR, LR, #0x04   ;确保使用的堆栈指针是PSP，而不是MSP
+    BX LR     ;退出异常
 }
 
 void tTaskRunFirst(void)
