e7107876258b5a0d258d323ced22d49d88618921
XZP <xingzhipeng2@126.com>
2018-03-20 21:25:27 +0800

    Update tList.c

--- uCOS/tList.c
+++ uCOS/tList.c
@@ -55,3 +55,54 @@ tNode * tListPre (tList * list, tNode * node)
 	}
 }
 
+tNode * tListNext (tList * list, tNode * node)
+{
+	if (node->nextNode == node)
+	{
+		return (tNode *)0;
+	}
+	else
+	{
+		return node->nextNode;
+	}
+}
+
+void tListRemoveAll (tList * list)
+{
+	uint32_t count;
+	tNode * nextNode;
+	
+	nextNode = list->firstNode;
+	for (count = list->nodeCount; count != 0; count--)
+	{
+		tNode * currentNode = nextNode;
+		nextNode = nextNode->nextNode;
+		
+		currentNode->nextNode = currentNode;
+		currentNode->preNode = currentNode;
+	}
+	
+	list->firstNode = &(list->headNode);
+	list->lastNode = &(list->headNode);
+	list->nodeCount = 0;
+}
+
+void tListAddFirst (tList * list, tNode * node)
+{
+	node->preNode = list->firstNode->preNode;
+	node->nextNode = list->firstNode;
+	
+	list->firstNode->preNode = node;
+	list->firstNode = node;
+	list->nodeCount++;
+}
+
+void tListAddLast (tList * list, tNode * node)
+{
+	node->nextNode = &(list->headNode);
+	node->preNode = list->lastNode;
+	
+	list->lastNode->nextNode = node;
+	list->lastNode = node;
+	list->nodeCount++;
+}
